8136b910ab1aa7b9fbf3aa6548355bc66d1e242e
calm <calm@calm.com>
2024-11-11 20:31:37 +0800

    init

---
+++ js/metamask.js
@@ -0,0 +1,42 @@
+// 检查网络
+async function checkChain() {
+    const {ethereum} = window;
+    // 判断链对不，链不对就请求切换网络，或者添加网络，
+    if (ethereum) {
+        try {
+            await ethereum.request({
+                method: 'wallet_switchEthereumChain',
+                params: [{
+                    chainId: Web3.utils.numberToHex(421614)
+        }]
+        })
+            MetaMaskInstalled = true;
+            MetaMaskConnected = true;
+        } catch (err) {
+            if (err.code === 4902) {
+                try {
+                    await ethereum.request({
+                        method: 'wallet_addEthereumChain',
+                        params: [
+                            {
+                                chainId: Web3.utils.numberToHex(421614),
+                    chainName: 'Arbitrum Sepolia',
+                        nativeCurrency: {
+                        name: 'ETH',
+                            symbol: 'ETH',
+                            decimals: 18
+                    },
+                    rpcUrls: ['https://sepolia-rollup.arbitrum.io/rpc'], // 节点
+                        blockExplorerUrls: ['https://sepolia.arbiscan.io/']
+                }
+                ]
+                })
+                } catch (ee) {
+                    console.log('Add chain failed.');
+                }
+            } else if (err.code === 4001) {
+                layer.msg("Please connect to MetaMask.")
+            }
+        }
+    }
+}
